09fb6d40e47004ca15d8b462076ebf2ed26f9da0
Zhen Yu <yu1@uni-bremen.de>
2023-01-26 19:31:51 +0100

    #508 BackButton

--- Assets/FaceMaskExample/WebCamTextureFaceMaskExample/WebCamTextureFaceMaskExample.cs
+++ Assets/FaceMaskExample/WebCamTextureFaceMaskExample/WebCamTextureFaceMaskExample.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b9d0cdf0e18bb35a8e8de4acf9664048882fe9fb07c3564e971973869e4f905
-size 36733
+oid sha256:5b74e571a94c820b7bf86d05a1e858924e6accde28beddf1e10f8c95bc4024d1
+size 36730
