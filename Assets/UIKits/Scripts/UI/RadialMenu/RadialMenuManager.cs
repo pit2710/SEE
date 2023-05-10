ca6ed319527970741f713958498f449926c008cd
Rainer Koschke <koschke@uni-bremen.de>
2023-05-10 09:43:08 +0200

    #596 A user can now select XRInputType.MouseBased to interact with a mouse. The original code uses conditional compilation. One must decide statically what environment to support, i.e., it was not possible to use a mouse to interact with VRUIKit when configured for XR. Now we can also use mouse input to hover and select the radial menu items.

--- Assets/UIKits/Scripts/UI/RadialMenu/RadialMenuManager.cs
+++ Assets/UIKits/Scripts/UI/RadialMenu/RadialMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd223bb6baabadb00887aae37481fb7c493928592a998bbe139407b673f3e8e8
-size 6621
+oid sha256:bc24e38833ff218baa4da247f7d3f1022dea81943be6cfecec134593714f8af4
+size 7329
