16aa1740f27f39bdbee8dbe1053b8196cf4ece67
koschke@uni-bremen.de <koschke@uni-bremen.de>
2023-05-10 18:26:22 +0200

    Let it compile.

--- Assets/UIKits/Scripts/UI/RadialMenu/RadialMenuManager.cs
+++ Assets/UIKits/Scripts/UI/RadialMenu/RadialMenuManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93af297ad160e58bcc3cfb8aefbad1be9761abd09f95354aca43ea4ea3f93fca
-size 8406
+oid sha256:055a00a9a83981452098e3cb98bba44f861d585367cebeac7dd1feecb6c417a8
+size 8410
